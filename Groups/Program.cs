c9d2d1e4dd3b2b5480516bf83f6f582d4384f306
tsvirenko <163934546+tsvirenko@users.noreply.github.com>
2024-03-19 21:53:14 +0200

    Update Program.cs

--- Groups/Program.cs
+++ Groups/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Groups
+﻿using Groups.Implementions;
+using Students.Implementations;
+namespace Groups
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+class Program {
+        static void Main(string[] args) {
+            var student1 = new StudentEntity { Id = 1, Name = "Mary", StudentId = "S001" };
+            var student2 = new StudentEntity { Id = 2, Name = "Stive", StudentId = "S002" };
             
+            var teacher = new TeacherEntity { Id = 1, Name = "Mrs. Klinson", TeacherId = "T001" };
+            
+            var group = new GroupEntity { Id = 1, Name = "English" };
+            group.AddStudent(student1);
+            group.AddStudent(student2);
+            group.AssignTeacher(teacher);
+
+            group.DisplayGroupInfo();
         }
     }
 }
