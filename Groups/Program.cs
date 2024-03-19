5afa7bd8205b084c5334db9025c7f97b833e0735
tsvirenko <163934546+tsvirenko@users.noreply.github.com>
2024-03-19 22:00:05 +0200

    Update Program.cs

--- Groups/Program.cs
+++ Groups/Program.cs
@@ -1,18 +1,18 @@
 ﻿using Groups.Implementions;
-using Students.Implementations;
+using Clients.Implementations;
 namespace Groups
 {
 class Program {
         static void Main(string[] args) {
-            var student1 = new StudentEntity { Id = 1, Name = "Mary", StudentId = "S001" };
-            var student2 = new StudentEntity { Id = 2, Name = "Stive", StudentId = "S002" };
+            var client1 = new ClientEntity { Id = 1, Name = "Mary", ClientId = "C1" };
+            var client2 = new ClientEntity { Id = 2, Name = "Stive", ClientId = "C2" };
             
-            var teacher = new TeacherEntity { Id = 1, Name = "Mrs. Klinson", TeacherId = "T001" };
+            var trainer = new TrainerEntity { Id = 1, Name = "Mrs. Klinson", TrainerId = "T001" };
             
-            var group = new GroupEntity { Id = 1, Name = "English" };
-            group.AddStudent(student1);
-            group.AddStudent(student2);
-            group.AssignTeacher(teacher);
+            var group = new GroupEntity { Id = 1, Name = "Stretching" };
+            group.AddClient(client1);
+            group.AddClient(client2);
+            group.AssignTrainer(trainer);
 
             group.DisplayGroupInfo();
         }
